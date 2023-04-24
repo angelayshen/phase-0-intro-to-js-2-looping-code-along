09c185a6c1ee551b80cf7064aa7a093743a90ba8
Angela Shen <angelayshen@gmail.com>
2023-04-24 00:28:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const names = ["Archer", "Simon", "Aiden"];
+const eventName = "wedding"
+const newArray = [];
+
+function writeCards(names, eventName) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    
+    return messages;
+}
+
+console.log(writeCards(names, eventName))
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
+
+console.log(countDown(20))
